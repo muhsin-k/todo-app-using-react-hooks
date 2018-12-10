134f3a16adb5191c892e7617b503d627e68f304e
muhzi4u <muhsinkeramam@gmail.com>
2018-12-10 19:35:51 +0530

    Updates

---
+++ precache-manifest.819e0249d3ae9fd284f3d82478efcb2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b71ae33c0ec6673869512a7d6307b604",
+    "url": "/todo-app-using-react-hooks/static/media/logo.b71ae33c.png"
+  },
+  {
+    "revision": "9a138c1672b6eb88f298",
+    "url": "/todo-app-using-react-hooks/static/js/runtime~main.9a138c16.js"
+  },
+  {
+    "revision": "d4192be4b6e56a61dcd7",
+    "url": "/todo-app-using-react-hooks/static/js/main.d4192be4.chunk.js"
+  },
+  {
+    "revision": "d7d5b1791e9a4186a415",
+    "url": "/todo-app-using-react-hooks/static/js/1.d7d5b179.chunk.js"
+  },
+  {
+    "revision": "d4192be4b6e56a61dcd7",
+    "url": "/todo-app-using-react-hooks/static/css/main.082a8e7a.chunk.css"
+  },
+  {
+    "revision": "d7d5b1791e9a4186a415",
+    "url": "/todo-app-using-react-hooks/static/css/1.615fc5dc.chunk.css"
+  },
+  {
+    "revision": "2e98896a93ff7232f1f9458562c46fb2",
+    "url": "/todo-app-using-react-hooks/index.html"
+  }
+];
